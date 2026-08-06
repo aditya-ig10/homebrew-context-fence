dff07ae9320666be929fde03c7aebef12bb7a69a
Aditya <93360691+aditya-ig10@users.noreply.github.com>
2026-08-06 17:05:13 +0530

    fix: correct v1.1.6-c cask sha256

--- Casks/context-fence.rb
+++ Casks/context-fence.rb
@@ -1,6 +1,6 @@
 cask "context-fence" do
   version "1.1.6-c"
-  sha256 "53112bd8148ead3e7d7ac8dec54e1a12ba88dc1472a530653f59887474c100cb0"
+  sha256 "53112bd8148ead3e7d7ac8dec54e1a12ba88dc1472a530653f59887474100cb0"
 
   url "https://github.com/aditya-ig10/homebrew-context-fence/releases/download/v#{version}/Context-Fence-#{version}-universal.dmg"
   name "Context Fence"
