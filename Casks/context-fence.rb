05cc7418b3e0e87272095c3d433443cb4345163d
Aditya <93360691+aditya-ig10@users.noreply.github.com>
2026-08-16 20:32:56 +0530

    add context-fence cask v1.1.8

--- Casks/context-fence.rb
+++ Casks/context-fence.rb
@@ -1,6 +1,6 @@
 cask "context-fence" do
-  version "1.1.7"
-  sha256 "97a9e1d07618c234b821a9a3b187e962c748554721a0e280dc2be47e3541757e"
+  version "1.1.8"
+  sha256 "3d7db7c85c196a852bd1baae5c143a56443bac85205fa20b232446457edae08a"
 
   url "https://github.com/aditya-ig10/homebrew-context-fence/releases/download/v#{version}/Context-Fence-#{version}-universal.dmg"
   name "Context Fence"
